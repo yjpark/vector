29d7a271526b18dba92f415f23c4cff6dfe71018
binarylogic <bjohnson@binarylogic.com>
2020-01-20 13:25:03 -0500

    wip
    Signed-off-by: binarylogic <bjohnson@binarylogic.com>

--- website/src/pages/components/sources/docker.js
+++ website/src/pages/components/sources/docker.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
+import Alert from '@site/src/components/Alert';
 import Heading from '@theme/Heading';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
@@ -16,31 +17,29 @@ function Community() {
   const {metadata: {team}} = siteConfig.customFields;
 
   return (
-    <Layout title="Collect Docker Logs, Metrics, & Events In Minutes" description="Collect Docker logs, metrics, and events in minutes, for free.">
+    <Layout title="Collect Docker Logs, Metrics, & Events In Minutes" description="Collect Docker logs, metrics, and events in minutes, for free. Quickly collect Docker logs and metrics and send them to one or more destinations.">
       <header className="hero domain-bg domain-bg--platforms">
         <div className="container container--narrow">
-          <div className="hero--component-icons">
-            <div class="icon panel">
+          <div className="component-icons">
+            <div className="icon panel">
               <img src="/img/logos/docker.png" alt="Docker" />
             </div>
+            <a href="#" className="icon panel" title="Select a destination">
+              <i className="feather icon-plus"></i>
+            </a>
           </div>
-          <h1>Collect Docker Logs, Metrics, &amp; Events In Minutes, For Free</h1>
-          <p>Written and maintained by the <Link to="/community/#team">Vector team</Link></p>
+          <h1>Collect Docker Logs &amp; Events In Minutes</h1>
+          <p>written and maintained, with love, by the <Link to="/community/#team">Vector team</Link></p>
         </div>
       </header>
-      <main className="container container--narrow margin-vert--xl">
+      <main className="container container--narrow long-form margin-vert--xl">
         <section>
-          <p>Collecting Docker logs and metrics is an essential task if you want to observe and monitor your Docker services, but doing so is easier said than done. How do you collect both logs and metrics? How do you enrich logs and metrics with the proper Docker context? And how do you so without spending hours reading manuals? We'll show you how in this guide.</p>
-        </section>
-        <section>
-          <AnchoredH2 id="about">About this guide</AnchoredH2>
+          <blockquote className="blockquote--primary">
+            <div>&quot;I just wanna, like, collect my Docker logs and grep them -- why is all of this so complicated?&quot;</div>
+            <footer>— all developers</footer>
+          </blockquote>
 
-          <ol>
-            <li>We'll use Vector to collect Docker logs _and_ metrics. Why Vector? See below.</li>
-            <li>We'll enrich all data collected with useful Docker context.</li>
-            <li>We'll show you how to send (fan-out) this data to any destination(s) you choose.</li>
-            <li>And we'll do it all in a few short minutes.</li>
-          </ol>
+          <p>Did you know that Docker splits long log messages? Or that collecting Docker logs, metrics, and events requires three separate APIs? Or that enriching this data with Docker context requires yet another API? Don't sweat it! We've got you covered. Below is a simple guide that uses a best-in-class tool (Vector) for collecting Docker logs and handling all of these edge cases. You'll be setup in minutes so you can get back to doing what you do best.</p>
         </section>
         <section>
           <AnchoredH2 id="guide">A simple step-by-step guide</AnchoredH2>
@@ -69,7 +68,7 @@ function Community() {
             </li>
 
             <li>
-              <AnchoredH3 id="configure">Choose one or more "sinks" (destinations)</AnchoredH3>
+              <AnchoredH3 id="configure">Add one or more "sinks" (destinations)</AnchoredH3>
 
               <p>fsdfdsf</p>
             </li>
